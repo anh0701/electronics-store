d46da951562351d5aa3b15703247e1c345b49514
Pham Thi Quynh Anh <quynhanhphamthi01@gmail.com>
2024-04-27 14:49:55 +0700

    rename variable

--- app/Http/Controllers/DoiMatKhau.php
+++ app/Http/Controllers/DoiMatKhau.php
@@ -12,31 +12,31 @@ class DoiMatKhau extends Controller
 {
     //
     public function DoiMatKhau(Request $request)
-{
-    $validator = Validator::make($request->all(), [
-        'Email' => ['required', 'string', 'Email', 'max:255'],
-        'MatKhau' => ['required', 'string', 'min:8'],
-    ]);
+    {
+        $validator = Validator::make($request->all(), [
+            'Email' => ['required', 'string', 'Email', 'max:255'],
+            'MatKhau' => ['required', 'string', 'min:8'],
+        ]);
 
-    if ($validator->fails()) {
-        return new JsonResponse(['success' => false, 'message' => $validator->errors()], 422);
-    }
+        if ($validator->fails()) {
+            return new JsonResponse(['success' => false, 'message' => $validator->errors()], 422);
+        }
 
-    $user = TaiKhoan::where('Email',$request->Email);
-    $user->update([
-        'MatKhau'=>md5($request->MatKhau)
-    ]);
+        $user = TaiKhoan::where('Email', $request->Email);
+        $user->update([
+            'MatKhau' => md5($request->MatKhau)
+        ]);
 
 //    $token = $user->first()->createToken('myapptoken')->plainTextToken;
 //        $token = 1;
-    return new JsonResponse(
-        [
-            'success' => true,
-            'message' => "Your password has been reset",
+        return new JsonResponse(
+            [
+                'success' => true,
+                'message' => "Your password has been reset",
 //            'token'=>$token
-        ],
-        200
-    );
-}
+            ],
+            200
+        );
+    }
 
 }
