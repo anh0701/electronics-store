f5e6872142eb67d258d884c3aa8f01a6f9513924
Pham Thi Quynh Anh <quynhanhphamthi01@gmail.com>
2024-05-03 11:41:39 +0700

    add file auth.css

--- resources/views/auth/datLaiMatKhau.blade.php
+++ resources/views/auth/datLaiMatKhau.blade.php
@@ -4,24 +4,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Đặt lại mật khẩu</title>
-    <link rel="stylesheet" href="{{ asset('/css/style_taiKhoan.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/auth.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.min.css') }}">
 </head>
 <body>
-    <div class="login-container">
+    <div class="container-custom">
         <h2>Đặt lại mật khẩu</h2>
         <form action="/dat-lai-mat-khau" method="POST">
             @csrf
-            <div class="error-message">
-                @if ($errors->any())
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                @endif
-            </div>
+{{--            <div class="error-message">--}}
+{{--                @if ($errors->any())--}}
+{{--                    <ul>--}}
+{{--                        @foreach ($errors->all() as $error)--}}
+{{--                            <li>{{ $error }}</li>--}}
+{{--                        @endforeach--}}
+{{--                    </ul>--}}
+{{--                @endif--}}
+{{--            </div>--}}
             <input type="password" name="MatKhauMoi" placeholder="Mật khẩu mới" value="{{ old('MatKhauMoi') }}">
+            @error('MatKhauMoi')
+            <div class="alert alert-danger">
+                {{ $message }}
+            </div>
+            @enderror
             <input type="password" name="MatKhauMoi2" placeholder="Nhập lại mật khẩu mới" value="{{ old('MatKhauMoi2') }}">
+            @error('MatKhauMoi2')
+            <div class="alert alert-danger">
+                {{ $message }}
+            </div>
+            @enderror
             <input type="submit" value="Đổi mật khẩu">
         </form>
 {{--        <a href="{{ route('dangKy') }}" class="link-dn">Chưa có tài khoản? Đăng ký!</a>--}}
